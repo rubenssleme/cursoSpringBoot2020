5ed4f15dfb9cf8ed04a5fa009dd783984f5347ab
rubenssleme <ajua614728>
2020-07-21 01:00:56 -0300

    Implementação de servico na Service layer, component registration

--- src/main/java/com/curso2020/curso/recursos/UsuarioRecurso.java
+++ src/main/java/com/curso2020/curso/recursos/UsuarioRecurso.java
@@ -1,20 +1,34 @@
 package com.curso2020.curso.recursos;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso2020.curso.entidades.Usuario;
+import com.curso2020.curso.servicos.UsuarioServico;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioRecurso {
 	
+	@Autowired
+	private UsuarioServico usuarioServico;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> findAll(){
-		Usuario u = new Usuario(1L, "Rubens", "rubens@", "99999999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>> findAll(){
+		List<Usuario> list = usuarioServico.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Usuario obj = usuarioServico.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
