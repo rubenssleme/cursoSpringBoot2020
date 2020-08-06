8f230ba7da8e24defb5a1481c7a10b493a2e105d
rubenssleme <ajua614728>
2020-08-06 11:10:07 -0300

    Deploy app to Heroku

--- src/main/java/com/curso2020/curso/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/curso2020/curso/repositorios/UsuarioRepositorio.java
@@ -1,9 +1,10 @@
 package com.curso2020.curso.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.curso2020.curso.entidades.Usuario;
-
+@Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
 
 }
