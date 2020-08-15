94f52c2f03070e6938d9173865cfd61041f8d58b
rubenssleme <ajua614728>
2020-08-15 09:26:08 -0300

    Pedido java e repositorio

--- src/main/java/com/curso2020/curso/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/curso2020/curso/repositorios/UsuarioRepositorio.java
@@ -1,7 +1,6 @@
 package com.curso2020.curso.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.curso2020.curso.entidades.Usuario;
 
